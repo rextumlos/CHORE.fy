032732c4a220fd784c727b874c7477bf5a590997
rextumlos <johnrex412@gmail.com>
2021-07-22 22:35:14 +0800

    Feature againnnnn

--- app/src/main/java/com/example/chorefy/activity_features_page.java
+++ app/src/main/java/com/example/chorefy/activity_features_page.java
@@ -1,14 +1,20 @@
 package com.example.chorefy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class activity_features_page extends AppCompatActivity {
 
     private ImageButton homeBtn, settingsBtn;
+    CardView cardRecipe;
+    CardView cardGrocery;
+    CardView cardPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,31 @@ public class activity_features_page extends AppCompatActivity {
 
         settingsBtn = (ImageButton) findViewById(R.id.imgBtnSettings);
         settingsBtn.setOnClickListener(v -> openSettingsActivity());
+
+        cardRecipe = findViewById(R.id.cardRecipe);
+        cardGrocery = findViewById(R.id.cardGrocery);
+        cardPayment = findViewById(R.id.cardPayment);
+
+        cardRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Recipe Clicked");
+            }
+        });
+
+        cardGrocery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Grocery Clicked");
+            }
+        });
+
+        cardPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Payment Clicked");
+            }
+        });
     }
 
     public void openHomeActivity(){
@@ -32,5 +63,10 @@ public class activity_features_page extends AppCompatActivity {
         Intent intent = new Intent(this, activity_settings_page.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
+    }
+
+    private void showToast(String message){
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
